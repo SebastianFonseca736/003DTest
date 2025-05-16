c9fcb48690520225d85d511aabfb3f964a1e1f0b
Sebastian <s.fonseca@duocuc.cl>
2025-05-16 18:26:49 -0400

    Prueba de Proyecto APIS

---
+++ src/main/java/com/example/_DTest/Controller/ApiController.java
@@ -0,0 +1,15 @@
+package com.example._DTest.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ApiController {
+    @GetMapping("/saludo")
+    public String hola() {
+        return "Hola Mundo";
+    }
+
+}
